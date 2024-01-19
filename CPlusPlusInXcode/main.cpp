6d1ad9a82043a050c61e90d225ce0ae3eebbd56c
Tamim Dari Chowdhury <dari.tamim028@gmail.com>
2024-01-19 23:55:42 +0600

    Initial commit

---
+++ CPlusPlusInXcode/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  CPlusPlusInXcode
+//
+//  Created by Tamim Dari Chowdhury on 1/9/24.
+//
+
+#include "stdc++.h"
+
+int main(int argc, const char * argv[]) {
+    
+    //freopen ("/Users/tamim028/swift/CPlusPlusInXcode/CPlusPlusInXcode/output.txt", "w", stdout);
+    
+    cout << "Hello, World\n";
+    
+    return 0;
+}
